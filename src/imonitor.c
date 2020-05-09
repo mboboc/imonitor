48fca1aa808ab166ee2a5157ce8d1ea7ff91561d
madalina.boboc <madalina.boboc@stud.acs.upb.ro>
2020-05-09 18:25:09 +0300

    dev #5

--- src/imonitor.c
+++ src/imonitor.c
@@ -2,6 +2,7 @@
 #include <pwd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/inotify.h>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -9,13 +10,29 @@
 
 #include "utils.h"
 
+/* watch /dev/pts */
+#define DIR "/dev/pts/"
+
 #define EVENT_SIZE (sizeof(struct inotify_event))
 #define EVENT_BUF_LEN (1024 * (EVENT_SIZE + 16))
 
-int main(void)
-{
+/* Get absolute path */
+char *get_path(char *dir, char *file) {
+    char *path;
+    size_t dir_size = strlen(dir);
+    size_t file_size = strlen(file);
+
+    path = malloc(sizeof(char) * (dir_size + file_size + 1));
+    strcpy(path, dir);
+    strcpy(&(path[dir_size]), file);
+
+    return path;
+}
+
+int main(void) {
     int fd, wd, rt;
     char buffer[EVENT_BUF_LEN];
+    char *path;
     struct stat status;
     struct passwd *pwd;
 
@@ -23,16 +40,25 @@ int main(void)
     fd = inotify_init();
     DIE(fd < 0, "[inotify_init] failed");
 
-    /*Adding the “me” directory into watch list */
-    wd = inotify_add_watch(fd, "/dev/pts", IN_CREATE | IN_DELETE);
+    /*Add DIR into watch list */
+    wd = inotify_add_watch(fd, DIR, IN_CREATE | IN_DELETE);
     DIE(wd < 0, "[inotify_add_watch] failed");
 
-    while (1) {
+    int i = 5;
+    while (i--) {
         /* Read to determine the event change happens on “/me” directory */
         rt = read(fd, buffer, EVENT_BUF_LEN);
         DIE(rt < 0, "[read] failed");
 
-        struct inotify_event *event = (struct inotify_event *)&buffer[0];
+        struct inotify_event *event = (struct inotify_event *)buffer;
+        path = get_path(DIR, event->name);
+
+        rt = stat(path, &status);
+        DIE(rt < 0, "[stat] failed");
+
+        pwd = getpwuid(status.st_uid);
+        DIE(pwd == NULL, "[getpwuid] failed");
+
         if (event->mask & IN_CREATE) {
             if (event->mask & IN_ISDIR)
                 printf("New directory %s created.\n", event->name);
@@ -44,15 +70,10 @@ int main(void)
             else
                 printf("File %s deleted.\n", event->name);
         }
-        stat(event->name, &status);
-        pwd = getpwuid(status.st_uid);
-        printf("%s\n", pwd->pw_name);
-        printf("Finished...\n");
     }
 
-    /* The directory from the watch list.*/
     inotify_rm_watch(fd, wd);
-
-    /*closing the INOTIFY instance*/
+    free(path);
+    free(pwd);
     close(fd);
 }
